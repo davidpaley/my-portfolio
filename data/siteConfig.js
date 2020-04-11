bcd8ecccdfcc42da29a91426cafd9ca8fa85bdda
davidpaley <david.paleyy@gmail.com>
2020-04-11 15:59:26 -0300

    Portfolio page working locally

---
+++ data/siteConfig.js
@@ -0,0 +1,159 @@
+module.exports = {
+  siteTitle: "Hi, I'm David!",
+  siteDescription: `David Paley's Portfolio`,
+  keyWords: ['gatsbyjs', 'react', 'curriculum'],
+  authorName: 'David Paley',
+  twitterUsername: 'PaleyDavid',
+  githubUsername: 'davidpaley',
+  authorAvatar: '/images/avatar.jpg',
+  authorDescription: `I describe myself as a proactive Software Engineer, someone who is 
+  consistently growing and takes the time to keep learning every day.<br />
+  I've been working remotely since I finished university in 2014, working for different companies
+  from different places (United States, Australia, Spain, etc).<br />My strongest field is the 
+  Frontend side, however, I also work
+  in the backend, currently with Node. I have been working with React JS since 2014, 
+  my main motivations are the 
+  performance improvements, clean code, serverless tools, among others.`,
+  skills: [
+    {
+      name: 'Javascript',
+      level: 95,
+    },
+    {
+      name: 'React',
+      level: 90,
+    },
+    {
+      name: 'HTML/CSS',
+      level: 70,
+    },
+    {
+      name: 'Web Performance',
+      level: 85,
+    },
+    {
+      name: 'Responsive Design',
+      level: 80,
+    },
+    {
+      name: 'Automated Tests',
+      level: 73,
+    },
+    {
+      name: 'Typescript',
+      level: 70,
+    },
+    {
+      name: 'Git',
+      level: 77,
+    },
+    {
+      name: 'NodeJs',
+      level: 40,
+    },
+    {
+      name: 'AWS',
+      level: 50,
+    },
+    {
+      name: 'SQL',
+      level: 35,
+    },
+    {
+      name: 'NoSQL databases',
+      level: 30,
+    },
+    {
+      name: 'Angular',
+      level: 60,
+    },
+  ],
+  jobs: [
+    {
+      company: 'AIME',
+      begin: {
+        month: 'Mar',
+        year: '2018',
+      },
+      occupation: 'Full Stack Developer',
+      description:
+        `<p>
+          I work for the Australian non profit remotely.
+          I develop with React js in the frontend side and with Node js in the backend. I also work 
+          making the designs possible with HTML and CSS (working with SASS, CSS Grid, Flexbox, etc).
+        </p>`
+    },
+    {
+      company: 'Freelance',
+      begin: {
+        month: 'Jun',
+        year: '2016',
+      },
+      duration: '2 years',
+      occupation: 'Freelance',
+      description:
+        `<p>
+          Working as Freelance I participated in several projects. Some of them are: <br />
+          - Helping building the TMP website. Application to manage tasks and track time on a 
+          collaborative environment. <br />- I developed a mobile App to implement in Uruguay, in order to 
+          promote some discounts for Direct TV. The application has a list of promotions that you have 
+          if you are subscribed with Direct TV, sorted by location and explained in detail.
+        </p>`
+    },
+    {
+      company: 'Strands',
+      begin: {
+        month: 'Mar',
+        year: '2015',
+      },
+      duration: '3 years',
+      occupation: 'Frontend Developer',
+      description:
+        `<p>
+          Built the Backoffice Product of PFM, used by the banks for customize their campaigns. 
+          Worked for the MoneyStrands project, developing frontend requirements. The first year I was 
+          responsible for the development of new features of the Strands Products: Recommendation 
+          Widgets, Clients Segmentations, Mail Service Campaigns for Web Retailers, maintenance of 
+          the Strands Dashboard. In the frontend with Javascript and in backend using Python, Django.
+        </p>`
+    },
+    {
+      company: 'Citibank',
+      begin: {
+        month: 'Oct',
+        year: '2012',
+      },
+      duration: '2 years and 5 months',
+      occupation: 'Full Stack Developer',
+      description:
+        `<p>
+          I was present in the full development life-cycle. Successfully built and maintained a 
+          variety of customer facing applications.Coded new modules in line with pre-agreed technical 
+          specifications. Worked closely with my partners during the tests in the UAT (User Acceptance 
+          Testing) environment and in the Production environment, to ensure a timely and successful 
+          outcome.
+        </p>`
+    },
+    /* ... */
+  ],
+  social: {
+    twitter: 'https://twitter.com/PaleyDavid',
+    linkedin: 'https://www.linkedin.com/in/davidpaley11/',
+    github: 'https://github.com/davidpaley',
+    email: 'david.paleyy@gmail.com',
+  },
+  siteUrl: 'https://santosfrancisco.github.io/gatsbystarter-cv',
+  pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
+  siteCover: '/images/cover.jpg',
+  googleAnalyticsId: 'UA-000000000-1',
+  background_color: '#ffffff',
+  theme_color: '#25303B',
+  display: 'minimal-ui',
+  icon: 'src/assets/gatsby-icon.png',
+  headerLinks: [
+    {
+      label: 'David Paley',
+      url: '/',
+    },
+  ],
+}
---
+++ src/components/contact/contact.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Row, Col } from 'react-awesome-styled-grid'
+import { ThemeProvider } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import SendIcon from '@material-ui/icons/Send'
+import { createMuiTheme } from '@material-ui/core/styles';
+import blueGrey from '@material-ui/core/colors/blueGrey';
+import grey from '@material-ui/core/colors/grey';
+
+const Contact = ({ className, title = 'contact me' }) => {
+  const customTheme = createMuiTheme({
+    palette: {
+      primary: blueGrey,
+      secondary: grey,
+    },
+    status: {
+      danger: 'red',
+    },
+  });
+  return (
+    <ThemeProvider theme={customTheme}>
+      <div className={className}>
+        <Row>
+          <h1>{title}</h1>
+        </Row>
+        <Row align="center" justify="center">
+          <form
+            method="post"
+            className="contact-form w100"
+            action="https://formspree.io/dani.montanaro21@gmail.com"
+          >
+            <Row className="w100">
+              <Col xs={4} md={4}>
+                <TextField
+                  className="text-input"
+                  id="name"
+                  name="name"
+                  label="Name"
+                  variant="outlined"
+                  required
+                />
+              </Col>
+              <Col xs={4} md={4}>
+                <TextField
+                  className="text-input"
+                  id="email"
+                  name="_replyto"
+                  label="Email"
+                  variant="outlined"
+                  required
+                />
+              </Col>
+            </Row>
+            <Row className="w100 text-area">
+              <TextField
+                className="w100"
+                id="message"
+                name="message"
+                label="Message"
+                variant="outlined"
+                multiline
+                rows="4"
+                required
+              />
+            </Row>
+            <Button
+              className="button"
+              size="large"
+              endIcon={<SendIcon />}
+              variant="contained"
+              type="submit"
+            >
+              SEND
+            </Button>
+          </form>
+        </Row>
+      </div>
+    </ThemeProvider>
+  )
+}
+
+export default styled(Contact)`
+  .w100 {
+    width: 100%;
+  }
+  .contact-form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .button {
+    margin-top: 2em;
+  }
+  
+  .text-input {
+    margin: 0 0 1.25em;
+    width: 100%;
+  }
+  .text-area {
+    padding-right: 0.75rem;
+    padding-left: 0.75rem;
+  }
+  @media (max-width: 1023px) {
+    .text-area {
+      width: 100%
+    }
+  }
+`
